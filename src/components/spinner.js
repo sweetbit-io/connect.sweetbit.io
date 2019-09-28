3d8fa3ba54ca70f703a494009a145e76100360f2
David Knezić <davidknezic@gmail.com>
2019-09-28 10:26:32 +0200

    Use inline elements in Spinner

--- src/components/spinner.js
+++ src/components/spinner.js
@@ -3,21 +3,21 @@ import React from 'react';
 const twelve = [...Array(12).keys()];
 
 export default () => (
-  <div className="spinner">
-    {twelve.map((i) => <div key={i} />)}
+  <span className="spinner">
+    {twelve.map((i) => <span key={i} />)}
     <style jsx>{`
       .spinner {
-        color: official;
         display: inline-block;
         position: relative;
         width: 24px;
         height: 24px;
       }
-      div {
+      .spinner span {
+        display: block;
         transform-origin: 12px 12px;
         animation: spinner 1.2s linear infinite;
       }
-      div:after {
+      .spinner span:after {
         content: " ";
         display: block;
         position: absolute;
@@ -28,51 +28,51 @@ export default () => (
         border-radius: 20%;
         background: currentColor;
       }
-      div:nth-child(1) {
+      .spinner span:nth-child(1) {
         transform: rotate(0deg);
         animation-delay: -1.1s;
       }
-      div:nth-child(2) {
+      .spinner span:nth-child(2) {
         transform: rotate(30deg);
         animation-delay: -1s;
       }
-      div:nth-child(3) {
+      .spinner span:nth-child(3) {
         transform: rotate(60deg);
         animation-delay: -0.9s;
       }
-      div:nth-child(4) {
+      .spinner span:nth-child(4) {
         transform: rotate(90deg);
         animation-delay: -0.8s;
       }
-      div:nth-child(5) {
+      .spinner span:nth-child(5) {
         transform: rotate(120deg);
         animation-delay: -0.7s;
       }
-      div:nth-child(6) {
+      .spinner span:nth-child(6) {
         transform: rotate(150deg);
         animation-delay: -0.6s;
       }
-      div:nth-child(7) {
+      .spinner span:nth-child(7) {
         transform: rotate(180deg);
         animation-delay: -0.5s;
       }
-      div:nth-child(8) {
+      .spinner span:nth-child(8) {
         transform: rotate(210deg);
         animation-delay: -0.4s;
       }
-      div:nth-child(9) {
+      .spinner span:nth-child(9) {
         transform: rotate(240deg);
         animation-delay: -0.3s;
       }
-      div:nth-child(10) {
+      .spinner span:nth-child(10) {
         transform: rotate(270deg);
         animation-delay: -0.2s;
       }
-      div:nth-child(11) {
+      .spinner span:nth-child(11) {
         transform: rotate(300deg);
         animation-delay: -0.1s;
       }
-      div:nth-child(12) {
+      .spinner span:nth-child(12) {
         transform: rotate(330deg);
         animation-delay: 0s;
       }
@@ -85,5 +85,5 @@ export default () => (
         }
       }
     `}</style>
-  </div>
+  </span>
 );
